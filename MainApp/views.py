c9a3df1af2d2003b5765d3b6f40158f4ae148ca7
Ivan Nestorik <vansilkn@mail.ru>
2025-09-09 23:10:31 +0300

    Done.M5:Snippets_task1-13

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from MainApp.models import Snippet
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index_page(request):
@@ -13,5 +15,18 @@ def add_snippet_page(request):
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    """ Получаем все элементы из базы данных. """
+    ls_snippets = Snippet.objects.all()
+    context = {'ls_snippets': ls_snippets}
     return render(request, 'pages/view_snippets.html', context)
+
+
+def get_snippets(request, snippets_id:int ):
+    """ Получаем элемент по идентификатору из базы данных. """
+    try:
+        snippets = Snippet.objects.get(id=snippets_id)
+    except ObjectDoesNotExist:
+        return render(request, "00000.html", {"errors": [f"""Сниппет с id={snippets_id} не найден"""]})
+    else:
+        context = {"snippets": snippets} 
+        return render(request, "pages/snippet_page.html", context)
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
 
+
 urlpatterns = [
-    path('', views.index_page, name=""),
-    path('snippets/add', views.add_snippet_page, name="add_snippet_page"),
-    path('snippets/list', views.snippets_page, name="snippets_page"),
+    path('', views.index_page, name="home"),
+    path('snippets/add', views.add_snippet_page, name="add_snippet"),
+    path('snippets/list', views.snippets_page, name="snippets_list"),
+    path('snippets/<int:snippets_id>/', views.get_snippets, name="snippets_detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
