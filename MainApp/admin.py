7e2555a66d8e86f99fe9690419827a8d6f22b9af
Ivan Nestorik <vansilkn@mail.ru>
2025-09-18 23:10:24 +0300

    Done.M9:Add Admin

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from MainApp.models import Snippet, Comment
 
 # Register your models here.
+# 1 variant
+# admin.site.register([Snippet, Comment])
+
+# 2 variant
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ["name", "lang", "creation_date", "public", "code"]
+    list_filter = ["lang", "creation_date"]
+    ordering = ["creation_date"]
+    search_filds = ["name", "code"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["text"]
+    search_filds = ["text"]
